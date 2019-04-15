fd0cb517e739d35bfe2892a79997293801ae9431
Kaliza <annkaliza@gmail.com>
2019-04-15 10:50:06 -0400

    edit page lesson

--- controllers/page.js
+++ controllers/page.js
@@ -2,19 +2,49 @@ const pageModel = require("../model/page");
 const navmenuModel = require("../model/navmenu");
 
 module.exports = class {
-	static async pageRoute(pageName, req, res, next) {
-		let page = await pageModel.getPage(pageName);
+	static async getPage(pagekey) {
+		let page = await pageModel.getPage(pagekey);
 		let navmenu = await navmenuModel.getNavmenu();
-
 		if (page[0]) {
-			page = page[0];
-			return res.render("root", {
-				title: page.title,
-				page: page,
-				nav: navmenu
+			return { title: page[0].title, page: page[0], nav: navmenu };
+		}
+		return false;
+	}
+
+	static async display(pageName, req, res, next) {
+		//pagedata = pd
+		let pd = await this.getPage(pageName);
+		if (pd) {
+			return res.render("page/display", {
+				title: pd.title,
+				page: pd.page,
+				nav: pd.nav
 			});
 		} else {
 			next();
 		}
+	}
+	static async edit(pageName, req, res, next) {
+		//pagedata = pd
+		let pd = await this.getPage(pageName);
+		if (pd) {
+			return res.render("page/edit", {
+				title: pd.title,
+				page: pd.page,
+				nav: pd.nav
+			});
+		} else {
+			next();
+		}
+	}
+
+	//save method
+	static async save(pagekey, req, res, next) {
+		const title = req.body.title;
+		const content = req.body.content;
+
+		await pageModel.savePage(pagekey, title, content);
+
+		await this.edit(pagekey, req, res, next);
 	}
 };
--- model/page.js
+++ model/page.js
@@ -10,4 +10,15 @@ module.exports = class {
 		connection.end();
 		return rows;
 	}
+
+	static async savePage(pagekey, title, content) {
+		let connection = await db.getConnection();
+		const result = await connection.query(
+			"INSERT INTO page (pagekey, title, content) values" +
+				"(?,?,?) ON DUPLICATE KEY UPDATE title=?, content=?",
+			[pagekey, title, content, title, content]
+		);
+		connection.end();
+		return result;
+	}
 };
